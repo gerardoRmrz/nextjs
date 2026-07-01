1366e5c25ba879a8fde162deb3ed0de411f72cb2
gerardoRmrz <grmzrso@outlook.com>
2026-07-01 17:49:45 -0500

    Server side validation to the registerUser Action in the blog app has been implemented

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -4,11 +4,42 @@ import { redirect } from "next/navigation";
 import bcrypt from "bcryptjs";
 import { db } from "../../db";
 import { users } from "../../db/schema";
+import { eq } from "drizzle-orm";
 
-export const registerUser = async (formData: FormData) => {
-  const username = (formData.get("username") as string)?.trim();
+export const registerUser = async (
+  prevState: {
+    errors: {};
+    values: { username: string; name: string; password: string };
+  },
+  formData: FormData,
+) => {
   const name = (formData.get("name") as string)?.trim();
+  const username = (formData.get("username") as string)?.trim();
   const password = formData.get("password") as string;
+
+  const errors = {};
+
+  if (!username || username.length < 4) {
+    (errors as any).username =
+      "The username must be at least 4 characters long";
+  }
+
+  if (!password || password.length < 4) {
+    (errors as any).password =
+      "The password must be at least 4 characters long";
+  }
+
+  const userExist = await db.query.users.findFirst({
+    where: eq(users.username, username),
+  });
+
+  if (!!userExist) {
+    (errors as any).userExist = "The username already exist";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors: { ...errors }, values: { username, name, password } };
+  }
 
   const passwordHash = await bcrypt.hash(password, 10);
 
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,29 +1,70 @@
-import Link from "next/link";
+"use client";
+
 import { registerUser } from "../actions/users";
+import { useActionState } from "react";
 
 export default function RegisterPage() {
+  const initialState = {
+    errors: {},
+    values: {
+      name: "",
+      username: "",
+      password: "",
+    },
+  };
+
+  const [state, formAction] = useActionState(registerUser, initialState);
+
+  const renderError = (key: string, indx: number) => {
+    if (Object.keys(state.errors).includes(key)) {
+      return (
+        <p style={{ color: "red" }}>
+          {Object.values(state.errors)[indx] as string}
+        </p>
+      );
+    }
+  };
+
   return (
     <div>
       <h2>Register</h2>
-      <form action={registerUser}>
+      <form action={formAction}>
         <div>
           <label>
             Username
-            <input type="text" name="username" required />
+            <input
+              type="text"
+              name="username"
+              required
+              defaultValue={state.values?.username}
+            />
+            {renderError("username", 0)}
           </label>
         </div>
         <div>
           <label>
             Name
-            <input type="text" name="name" required />
+            <input
+              type="text"
+              name="name"
+              required
+              defaultValue={state.values?.name}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input type="password" name="password" required />
+            <input
+              type="password"
+              name="password"
+              required
+              defaultValue={state.values?.password}
+            />
+            {renderError("password", 1)}
           </label>
         </div>
+        {renderError("userExist", 2)}
         <button type="submit">Register</button>
       </form>
     </div>
