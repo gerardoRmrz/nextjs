ba15dc4402c812108b6ed6a477da742ffd990a69
gerardoRmrz <grmzrso@outlook.com>
2026-07-09 17:11:11 -0500

    The routes /api/testing/reset and /api/testing/users have been implemented

---
+++ app/api/testing/reset/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/db";
+import { users, blogs, readingLists } from "@/db/schema";
+
+export const DELETE = async (req: NextRequest) => {
+  if (process.env.NODE_ENV === "production") {
+    return NextResponse.json(
+      { error: "This endpoint is not available in production" },
+      { status: 403 },
+    );
+  }
+  try {
+    await db.delete(readingLists);
+    await db.delete(blogs);
+    await db.delete(users);
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+};
---
+++ app/api/testing/users/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import bcrypt from "bcryptjs";
+
+export const POST = async (req: NextRequest) => {
+  if (process.env.NODE_ENV === "production") {
+    return NextResponse.json(
+      { error: "This endpoint is not available in production" },
+      { status: 403 },
+    );
+  }
+
+  const {
+    username,
+    name,
+    password,
+  }: { username: string; name: string; password: string } = await req.json();
+
+  try {
+    if (!username || !name || !password) {
+      return NextResponse.json(
+        { error: "The data is incomplete" },
+        { status: 400 },
+      );
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const token = "";
+
+    await db.insert(users).values({ username, name, passwordHash, token });
+
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+};
