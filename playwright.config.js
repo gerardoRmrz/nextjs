d3f4e503b069c498cf40f216831bbc3de01421bf
gerardoRmrz <grmzrso@outlook.com>
2026-07-13 19:38:47 -0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ const { defineConfig, devices } = require("@playwright/test");
 
 module.exports = defineConfig({
   testDir: "./tests",
-  timeout: 250 * 1000,
+  timeout: 350 * 1000,
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
